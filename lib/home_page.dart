aa5e530170a48fd234a50bf1d6e78ce786e1cf95
haseary <hase.ray.business@gmail.com>
2023-08-29 04:32:49 +0900

    function of youtube complete

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,18 +14,27 @@ class _HomePageState extends State<HomePage> {
    List<Color> colors = [
     const Color.fromRGBO(250, 100, 100, 1),
     const Color.fromRGBO(250, 140, 60, 1),
-    const Color.fromRGBO(90, 255, 109, 1),
+    const Color.fromRGBO(90, 255, 110, 1),
     const Color.fromRGBO(90, 145, 255, 1),
-    const Color.fromRGBO(184, 91, 255, 1),
+    const Color.fromRGBO(185, 90, 255, 1),
    ];
    double? _deviceWidth, _deviceHeight;
    String? _categories = "";
    List _press = [];
    List _categoriesJson = [];
+   YoutubePlayerController youtubeController = YoutubePlayerController(
+    initialVideoId: '4b6DuHGcltI',
+    flags: YoutubePlayerFlags(
+        autoPlay: false,  // 自動再生しない
+      ),
+    );
+   bool displayYoutube = true;
+   
    List<Widget> _videoCells = [];
    Map<String, double> layout_height = {};
    String _category_name = "ビジネス";
    Color _color = Color.fromRGBO(250, 100, 100, 1);
+
    final prefs = SharedPreferences.getInstance();
   @override
   void initState() {
@@ -59,6 +68,27 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void openYoutube(String youtube_id) async {
+      var padding = MediaQuery.of(context).padding;
+      double innerHeight = _deviceHeight! - padding.top - padding.bottom;
+      layout_height['youtube_display'] = _deviceWidth!/16*9;
+      layout_height['news_cells'] = 0;
+      layout_height['news_cells'] = innerHeight - layout_height.values.reduce((a, b) => a + b) - 2;
+      displayYoutube = true;
+      await Future.delayed(Duration.zero);
+      youtubeController.load( youtube_id,startAt:0);
+  }
+
+  void closeYoutube(){
+    var padding = MediaQuery.of(context).padding;
+    double innerHeight = _deviceHeight! - padding.top - padding.bottom;
+    layout_height['youtube_display'] = 0;
+    layout_height['news_cells'] = 0;
+    layout_height['news_cells'] = innerHeight - layout_height.values.reduce((a, b) => a + b) - 2;
+    youtubeController.pause();
+    displayYoutube = false;
+  }
+
   double fontSize(int text_count) {
     double fontSize = 0;
     if(text_count < 4){
@@ -75,12 +105,7 @@ class _HomePageState extends State<HomePage> {
       _color =  colors[category_num % colors.length];
       _videoCells = [];
       _press = json.decode(_categoriesJson[category_num]['press']);
-      print("OK");
-      for(var i=0; i < _press.length; i++){
-        _videoCells.add(
-          VideoCell(i)
-        );
-      }
+      closeYoutube();
     });
   }
 
@@ -97,38 +122,46 @@ class _HomePageState extends State<HomePage> {
             child: Row(
               children: [
                 Container(
-                  width: cellWidth/2,
+                  width: cellWidth / 2,
                   height: cellHeight,
                   color: Colors.red,
-                  child:Stack(
-                    children: [
-                      Positioned.fill(
-                        child: 
-                        Image.network(
-                          "http://img.youtube.com/vi/$youtube_id/sddefault.jpg",
-                          fit: BoxFit.cover,
-                          errorBuilder: (c, o, s) {
-                            return const Icon(
-                              Icons.error,
-                              color: Colors.red,
-                            );
-                          },
+                  child: InkWell(
+                    onTap: () {
+                      // onPressed イベントの処理をここに書きます
+                      print('Container tapped!');
+                      setState(() {
+                        openYoutube(youtube_id);
+                        youtubeController.load( youtube_id,startAt:0);
+                      });
+                    },
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: Image.network(
+                            "http://img.youtube.com/vi/$youtube_id/sddefault.jpg",
+                            fit: BoxFit.cover,
+                            errorBuilder: (c, o, s) {
+                              return const Icon(
+                                Icons.error,
+                                color: Colors.red,
+                              );
+                            },
+                          ),
                         ),
-                      ),
-                      Positioned.fill(
-                        child: Center(
-                          child: Opacity(
-                            opacity: 0.5,
-                            child: Icon(
-                              Icons.play_circle,
-                              size: 50,
-                              color: Colors.white,
+                        Positioned.fill(
+                          child: Center(
+                            child: Opacity(
+                              opacity: 0.5,
+                              child: Icon(
+                                Icons.play_circle,
+                                size: 50,
+                                color: Colors.white,
+                              ),
                             ),
-                          )
-                          
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
                 Container(
@@ -167,10 +200,14 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     _deviceWidth = MediaQuery.of(context).size.width;
     _deviceHeight = MediaQuery.of(context).size.height;
+    double? youtubeHeight = layout_height['youtube_display'];
+    //youtubeController.reload();
     final container_width = _deviceWidth!/5;
     final container_height = layout_height['category_bar'];
     _categoriesJson = json.decode(_categories!);
@@ -193,56 +230,61 @@ class _HomePageState extends State<HomePage> {
             child: Row(
               children: [
                 for (var i = 0; i < _categoriesJson.length; i++)
-                  Container(
-                    color: colors[i % colors.length],
-                    width: container_width,
-                    height: container_height,
-                    padding: EdgeInsets.all(0),
-                    margin: EdgeInsets.all(0),
-                    child:TextButton(
-                      onPressed: () {
-                        SelectCategory(i);
-                      },
-                      child: Text(
-                        _categoriesJson[i]['japanese_name'],
-                        style: TextStyle(
-                          fontSize: fontSize(_categoriesJson[i]['japanese_name'].length),
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
+                Container(
+                  color: colors[i % colors.length],
+                  width: container_width,
+                  height: container_height,
+                  padding: EdgeInsets.all(0),
+                  margin: EdgeInsets.all(0),
+                  child:TextButton(
+                    onPressed: () {
+                      SelectCategory(i);
+                    },
+                    child: Text(
+                      _categoriesJson[i]['japanese_name'],
+                      style: TextStyle(
+                        fontSize: fontSize(_categoriesJson[i]['japanese_name'].length),
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
                       ),
-                      style: TextButton.styleFrom(
-                        padding: EdgeInsets.all(0), // ボタンの内側の余白
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0), // 角丸の半径
-                        ),
+                    ),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.all(0), // ボタンの内側の余白
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0), // 角丸の半径
                       ),
-                    )
-                  ),
+                    ),
+                  )
+                ),
               ],
             ),
           ),
           Container(
-                    color: _color,
-                    width: _deviceWidth,
-                    height: layout_height['category_bar_line'],
-                    ),
-          YoutubePlayer(
-            controller: YoutubePlayerController(
-              initialVideoId: 'v2Vu_Of9exk',  // YouTube 動画の ID を指定
-              flags: YoutubePlayerFlags(
-                autoPlay: false,  // 自動再生しない
-              ),
+            color: _color,
+            width: _deviceWidth,
+            height: layout_height['category_bar_line'],
             ),
-            showVideoProgressIndicator: true,
-            progressIndicatorColor: Colors.blueAccent,
+          Container(
+            height: layout_height['youtube_display'],
+            color: Colors.red,
+            child:
+            YoutubePlayer(
+              controller: youtubeController,
+              //controller: YoutubePlayerController(initialVideoId: youtubeId),
+              showVideoProgressIndicator: true,
+              progressIndicatorColor: Colors.blueAccent,
+            ),
           ),
           Container(
             height: layout_height['news_cells'],
             //margin: EdgeInsets.only(bottom: 100),
             child: SingleChildScrollView(
               child: Column(
-                children: _videoCells,
+                //children: _videoCells,
+                children: [
+                  for (var i = 0; i < _press.length; i++)
+                    VideoCell(i)
+                ],
               ),
             )
           ),
